b90d98977b96a275c587249e0499ac4990e21d43
josganesh <116706998+josganesh@users.noreply.github.com>
2023-03-27 13:50:31 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,7 +1,7 @@
 public class file {
     public static void main(String[] args) {
         System.out.println("Hello, World! hi ganesh"); 
-        System.out.println("good mrng Hello, hi ganesh"); 
+        System.out.println("good mrng Hello, hi ganesh raja"); 
 
     }
 }
